e7eb65150771126e9ebdd730c60946721b07470f
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2024-12-04 10:59:28 +1100

    day 2 part 2 rust (incorrect!)

--- rust/day2/src/main.rs
+++ rust/day2/src/main.rs
@@ -24,6 +24,37 @@ fn main() {
             { 1 } else { 0 }; //this result of 1 or 0 makes it easier to just count the number of safe reports in the next step
     }
 
-    let safe_count = words.map(safe).reduce(|acc, curr| acc+curr ).unwrap();
-    println!("{:?}", safe_count);
+    let safe_count = words.clone().map(safe).reduce(|acc, curr| acc+curr ).unwrap();
+    println!("Part 1: {:?}", safe_count);
+
+    fn allCombinations<I>(arr: I) -> Vec<Vec<i32>>
+        where 
+            I: IntoIterator<Item = i32> + Clone
+    {
+        let len = arr.clone().into_iter().size_hint().0;
+        //create a bunch of copies of arr, each with the element at position n removed
+        let mut n = 0;
+        return std::iter::from_fn(move || {
+            n+=1;
+            if n >= len {
+                return None;
+            }
+            //this part i needed ai, as I didn't know how to use chain yet
+            //I also don't know why this needs to be wrapped in Some (I guess its the from_fn needs to do None/Some)
+            //I also don't know if the many clones here are dangerous/expensive
+            return Some(arr.clone().into_iter().take(n-1).chain(arr.clone().into_iter().skip(n)).collect::<Vec<_>>());
+
+            //here was my attempt with a skip of 1 but didn't work
+            //return Some(arr.clone().into_iter().take(n-1).skip(1).take(len).collect::<Vec<_>>());
+        }).collect::<Vec<_>>()
+    }
+    fn anySafeCombination<I>(arr: I) -> i32
+        where  
+            I: IntoIterator<Item = Vec<i32>> + Clone
+    {
+        return arr.clone().into_iter().map(safe).reduce(|acc, curr| acc+curr).unwrap().signum(); //returning 1 or 0 here
+    }
+
+    let safe_count = words.clone().map(allCombinations).map(anySafeCombination).reduce(|acc, curr| acc+curr ).unwrap();
+    println!("Part 2: {:?}", safe_count);
 }
