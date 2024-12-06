70ba07b91540f4e092e2c30ca4c3c49112e6ecd2
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2024-12-06 11:55:34 +1100

    day 5 part 1 with broken part 2

--- utils.js
+++ utils.js
@@ -69,3 +69,20 @@ export const isSuperset = function(set, subset) {
     }
     return _difference;
   }
+
+  //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  export const shuffle = function(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  }
