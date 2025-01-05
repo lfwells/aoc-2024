be27b649e2f1a5c8a1fbe109bfffa549a64c51ad
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2025-01-05 23:46:03 +1100

    day 8 part 2

--- day8.js
+++ day8.js
@@ -24,54 +24,67 @@ run(8, (input) =>
             }
         }
     }
-    console.log({ants});
 
     function inBounds(x,y)
     {
         return x >= 0 && x < width && y >= 0 && y < height;
     }
 
-    let antiAnts = [];
-    function getAntiAnts(ant1, ant2)
+    function getAntiAnts(ant1, ant2, part1)
     {
         let dx = Math.round(ant2.x - ant1.x);
         let dy = Math.round(ant2.y - ant1.y);
-        let antiAnts = [
-            {x: ant1.x - dx, y: ant1.y - dy},
-            {x: ant2.x + dx, y: ant2.y + dy}//,
-            //{x: ant1.x - dx*2, y: ant1.y - dy*2},
-            //{x: ant2.x + dx*2, y: ant2.y + dy*2}
-        ];
+        let antiAnts = [];
+        for (var i = 1; i < (part1 ? 2 : 1000); i++)
+        {
+            antiAnts = [...antiAnts, 
+                {x: ant2.x - dx*i, y: ant2.y - dy*i},
+                {x: ant1.x + dx*i, y: ant1.y + dy*i}
+            ];
+        }
         antiAnts = antiAnts.filter(a => inBounds(a.x, a.y));
         return antiAnts;
     }
     
-    let count = 0;
-    for (var letter of antLetters)
+    function run(part1)
     {
-        let antsThisLetter = ants.filter(a => a.c == letter);
-        let combs = [];
-        for (var i = 0; i < antsThisLetter.length; i++)
+        let count = 0;
+        for (var letter of antLetters)
         {
-            for (var j = i+1; j < antsThisLetter.length; j++)
+            let antsThisLetter = ants.filter(a => a.c == letter);
+            let combs = [];
+            for (var i = 0; i < antsThisLetter.length; i++)
             {
-                combs.push([antsThisLetter[i], antsThisLetter[j]]);
+                for (var j = i+1; j < antsThisLetter.length; j++)
+                {
+                    combs.push([antsThisLetter[i], antsThisLetter[j]]);
+                }
+            }
+            let antiAnts = combs.map(c => getAntiAnts(c[0], c[1], part1)).flat();
+            count += antiAnts.length;  
+            for (var ant of antiAnts)
+            {
+                //if (map[ant.y][ant.x] != ".") count--;
+                //else 
+                debugMap[ant.y][ant.x] = "~";
             }
         }
-        let antiAnts = combs.map(c => getAntiAnts(c[0], c[1])).flat();
-        count += antiAnts.length;  
-        for (var ant of antiAnts)
-        {
-            if (map[ant.y][ant.x] != ".") count--;
-            //else 
-            debugMap[ant.y][ant.x] = "X";
-        }
+
+        //ugh just count how many ~s in the debugMap then
+        count = debugMap.map(x => x.filter(y => y == "~").length).reduce((a,b) => a+b);
+        return count;
     }
-    console.log({part1:count});
+
+    console.log({part1:run(true)});
     //console.log(printMap(debugMap));
     //231 too low ....
     //254 too high....
     //240 was correct, i literally had to guess this :(
+    console.log({part2:run(false)});
+    //838 too low...
+    //859 too low...
+    //1043 too high...
+    //955 was correct but only by breaking part 1 lol...
 
     
     function printMap(map)
